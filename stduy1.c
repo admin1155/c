28b4db2cf709f086adf0ee52514132d2cbf34b1e
xmccln <xmccln@outlook.com>
2024-10-08 21:01:20 +0800

    edit

--- stduy1.c
+++ stduy1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main(){
+    /*p31 13 */
     printf("  @  \n");
     printf("@@$@@\n");
     printf("  @  \n");
+    printf("________________________\n");
+    /*p31 14*/
+    printf("**\n");
+    printf("###\n");
+    printf("3333\n");
+    printf("________________________\n");
+    /*p32 22*/
+    printf("¡ú¡ú¡ú¡ñ¡û¡û¡û\n")
 }
